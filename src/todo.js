aa7bbeee65fa9bce021db5de17879faa7fab5372
Jonathan <j.sweet90@hotmail.com>
2025-04-01 06:50:38 -0500

    Create Todo class

---
+++ src/todo.js
@@ -0,0 +1,48 @@
+export class Todo {
+    #title = "";
+    #description = "";
+    #dueDate;
+    #priority; // 0 - Lowest Priority; 1 - Normal Priority; 2 - Highest Priority
+
+    constructor(title, desc, dueDate=null, priority=1) {
+        this.#title = title;
+        this.#description = desc;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+    }
+
+    // Getters
+    getTitle() {
+        return this.#title;
+    }
+
+    getDesc() {
+        return this.#description;
+    }
+
+    getDueDate() {
+        // TODO: Format this into an easy to read string
+        return this.#dueDate;
+    }
+
+    getPriority() {
+        return this.#priority;
+    }
+
+    // Setters
+    changeTitle(newTitle) {
+        this.#title = newTitle;
+    }
+
+    changeDesc(newDesc) {
+        this.#description = newDesc;
+    }
+
+    changeDueDate(newDate) {
+        this.#dueDate = newDate;
+    }
+
+    changePriority(newPriority) {
+        this.#priority = newPriority;
+    }
+}
