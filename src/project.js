11b83b750cd1f705f82a3fff2cf619036c440f08
Jonathan <j.sweet90@hotmail.com>
2025-04-01 07:30:36 -0500

    Create Project class

---
+++ src/project.js
@@ -0,0 +1,59 @@
+import { Todo } from "./todo";
+
+export class Project {
+    #name = "";
+    #todos = [];
+    #color = "";
+
+    constructor(name="Default", color="white") {
+        this.#name = name;
+        this.#color = color;
+    }
+
+    addTodo(title, desc, dueDate=null, priority=1) {
+        this.#todos.push(new Todo(title, desc, dueDate, priority));
+    }
+
+    removeTodo(index) {
+        if(index < 0 || index > this.#todos.length) {
+            console.error("Requested Todo index out of bounds");
+            return;
+        }
+        this.#todos = this.#todos.filter((todo, i) => { if(i != index) todo; });
+    }
+
+    // Getters
+    getName() {
+        return this.#name.toUpperCase();
+    }
+
+    getTodoList() {
+        return this.#todos;
+    }
+
+    getTodo(index) {
+        if(index < 0 || index > this.#todos.length) {
+            console.error("Requested Todo index out of bounds");
+            return;
+        }
+        return this.#todos[index];
+    }
+
+    getTodoTitles() {
+        const titles = this.#todos.map(todo => todo.getTitle());
+        return titles;
+    }
+
+    getColor() {
+        return this.#color;
+    }
+
+    // Setters
+    changeName(newName) {
+        this.#name = newName;
+    }
+
+    changeColor(newColor) {
+        this.#color = newColor;
+    }
+}
